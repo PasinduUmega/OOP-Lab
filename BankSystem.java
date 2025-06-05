b161d1c08612817b518278e0e7f83570c1051a5c
Pasindu Umega <pasinduumega911@gmail.com>
2025-06-05 12:11:50 +0530

    Add files via upload

--- BankSystem.java
+++ BankSystem.java
@@ -1,81 +1,73 @@
-import java.util.Scanner;
-class Bank{
-    private String accountHolderName;
-    private int accountNumber;
-    private double balance;
-
-// Constructor to initialized for the bank class
-   public Bank(String accountHolderName,int accountNumber, double balance){
-       this.accountHolderName = accountHolderName;
-       this.accountNumber = accountNumber;
-       this.balance = balance < 0 ? 0 : balance; // Ensuring the balance for the not negative value
-   }
-
-// Create the deposit methods
-    public void depositAmounts(double amount){
-       if(amount> 0){
-           balance += amount;
-           System.out.println("New Balance:" + balance);
-       }else{
-
-       }
-    }
-
-// Create the withdraw methods
-   public void withdrawAmounts(double amount){
-       if(amount>0 && amount<= balance){
-           balance -= amount;
-           System.out.println("New Balance"+balance);
-       } else if (amount> balance) {
-           System.out.println("Insufficient for your account balance");
-       }else {
-           System.out.println("Withdraw amount not positive");
-       }
-   }
-
-// Display the bank details
-   public void displayBankDetails(){
-       System.out.println("\n Account Details");
-       System.out.println("Account Number:" + accountNumber);
-       System.out.println("Account Holder Name:" + accountHolderName);
-       System.out.println("Current Balance for the account:" + balance);
-   }
+interface InterApplicable{
+    double calculateInterest();
 }
-public class BankSystem {
+abstract class Account{
+    int accountNumber;
+    String accountHolderName;
+    double balance;
+    static String bankName = "National Savings Bank";
+
+   public Account(int accountNumber,String accountHolderName,double balance){
+       this.accountNumber = accountNumber;
+       this.accountHolderName = accountHolderName;
+       this.balance = balance;
+   }
+  abstract void displayAccountInfo();
+   }
+
+   class SavingsAccount extends Account implements InterApplicable{
+    double interestRate = 0.05;
+
+    public SavingsAccount(int accountNumber,String accountHolderName,double balance){
+        super(accountNumber,accountHolderName,balance);
+    }
+    @Override
+       public double calculateInterest(){
+        return balance * interestRate;
+    }
+    @Override
+       public void displayAccountInfo(){
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Account Holder Name: " + accountHolderName);
+        System.out.println("Account Balance: " + balance);
+        System.out.println("Interest Rate: " + interestRate);
+    }
+   }
+ class FixedDepositeAccount extends  Account implements InterApplicable{
+    double fixedDepositInterestRate =  0.1;
+    public FixedDepositeAccount (int accountNumber,String accountHolderName,double balance){
+        super(accountNumber,accountHolderName,balance);
+    }
+     @Override
+     public double calculateInterest(){
+         return balance * fixedDepositInterestRate;
+     }
+    @Override
+     public void displayAccountInfo(){
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Account Holder Name: " + accountHolderName);
+        System.out.println("Account Balance: " + balance);
+        System.out.println("Fixed Deposit Rate : " + fixedDepositInterestRate);
+    }
+ }
+
+public class BankSystem{
     public static void main(String[] args){
 
-// Create the scanner object to the read
-   Scanner scanner = new Scanner(System.in);
+     SavingsAccount savingsAccount = new SavingsAccount(1019,"Jon Smith",50000);
+     FixedDepositeAccount fixedDepositeAccount = new FixedDepositeAccount(1089,"Michell Clerk",60000);
 
-// Take the user input for adding the system
-   System.out.print("Enter the Account Number:");
-   int accountNumber = scanner.nextInt();
-   scanner.nextLine();
+     savingsAccount.displayAccountInfo();
+     System.out.println();
+     fixedDepositeAccount.displayAccountInfo();
+     System.out.println();
 
-   System.out.print("Enter the Account Holder Name:");
-   String accountHolderName = scanner.nextLine();
+     Account.bankName = "Bank Of Ceylon";
 
-
-   System.out.print("Enter the current Balance:");
-   double balance = scanner.nextDouble();
-
-// Create the bank object
-    Bank bank = new Bank(accountHolderName,accountNumber,balance);
-
-// Allowing the deposit amount for the account
-   System.out.print("Enter the Deposit Amount:");
-   double depositAmount = scanner.nextDouble();
-// Display the deposit methods
-   bank.depositAmounts(depositAmount);
-
-// Allowing the withdraw amount for the account
-    System.out.print("Enter the Withdraw Amount:");
-    double withdrawAmount = scanner.nextDouble();
-// Display the withdraw methods
-    bank.withdrawAmounts(withdrawAmount);
-
-// Display the bank details
-   bank.displayBankDetails();
+        System.out.println("After changing the name:");
+        savingsAccount.displayAccountInfo();
+        System.out.println();
+        fixedDepositeAccount.displayAccountInfo();
 
     }
 }
