7011e3358225216adcd6c87ede6757fb36ff7fda
Pasindu Umega <pasinduumega911@gmail.com>
2025-06-30 12:53:50 +0530

    Add files via upload

--- BankSystem.java
+++ BankSystem.java
@@ -1,73 +1,58 @@
-interface InterApplicable{
-    double calculateInterest();
+package Bank;
+class Bank{
+	private String accountHolderName;
+	private double depositeAmounts;
+	private double withdrowAmounts;
+	private double accountBalance;
+	
+// Constructor to initialized for the bank class	
+	public Bank(String accountHolderName, double depositeAmounts,double withdrowAmounts, double accountBalance) {
+		this.accountHolderName = accountHolderName;
+		this.depositeAmounts = depositeAmounts;
+		this.withdrowAmounts = withdrowAmounts;
+		this.accountBalance = accountBalance;
+	}
+	
+// Display the bank Details 
+	public void displayBankDetails() {
+
+		System.out.println("Account Holder Name:" + accountHolderName);
+		System.out.println("Deposite Amounts:" +  depositeAmounts);
+		System.out.println("Withdrow Amounts:" + withdrowAmounts);
+		System.out.println("Account Balance:" + accountBalance);
+	}
+
+	
+// 	Account Holder Name class (child class of Bank)
+	class accountHolderName extends Bank{
+	public accountHolderName( double depositeAmounts, double withdrowAmounts,
+			double accountBalance) {
+		super("accountHolderName", depositeAmounts, withdrowAmounts, accountBalance);
+		
+	}
+  }
+
+// @Override
+	public void accountNumber() {
+		System.out.println("Account Number: 101010");
+	}
 }
-abstract class Account{
-    int accountNumber;
-    String accountHolderName;
-    double balance;
-    static String bankName = "National Savings Bank";
 
-   public Account(int accountNumber,String accountHolderName,double balance){
-       this.accountNumber = accountNumber;
-       this.accountHolderName = accountHolderName;
-       this.balance = balance;
-   }
-  abstract void displayAccountInfo();
-   }
+public class BankSystem {
 
-   class SavingsAccount extends Account implements InterApplicable{
-    double interestRate = 0.05;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//  Create the bank object
+		Bank bank =  new Bank("Jon", 50000.0,20000.0,30000.0);
+		
+// Display the bank details
+		System.out.println("--Welcome our Bank System--");
+		bank.displayBankDetails();
+		bank.accountNumber();
+	
+		
 
-    public SavingsAccount(int accountNumber,String accountHolderName,double balance){
-        super(accountNumber,accountHolderName,balance);
-    }
-    @Override
-       public double calculateInterest(){
-        return balance * interestRate;
-    }
-    @Override
-       public void displayAccountInfo(){
-        System.out.println("Account Number: " + accountNumber);
-        System.out.println("Account Holder Name: " + accountHolderName);
-        System.out.println("Account Balance: " + balance);
-        System.out.println("Interest Rate: " + interestRate);
-    }
-   }
- class FixedDepositeAccount extends  Account implements InterApplicable{
-    double fixedDepositInterestRate =  0.1;
-    public FixedDepositeAccount (int accountNumber,String accountHolderName,double balance){
-        super(accountNumber,accountHolderName,balance);
-    }
-     @Override
-     public double calculateInterest(){
-         return balance * fixedDepositInterestRate;
-     }
-    @Override
-     public void displayAccountInfo(){
-        System.out.println("Account Number: " + accountNumber);
-        System.out.println("Account Holder Name: " + accountHolderName);
-        System.out.println("Account Balance: " + balance);
-        System.out.println("Fixed Deposit Rate : " + fixedDepositInterestRate);
-    }
- }
+	}
 
-public class BankSystem{
-    public static void main(String[] args){
-
-     SavingsAccount savingsAccount = new SavingsAccount(1019,"Jon Smith",50000);
-     FixedDepositeAccount fixedDepositeAccount = new FixedDepositeAccount(1089,"Michell Clerk",60000);
-
-     savingsAccount.displayAccountInfo();
-     System.out.println();
-     fixedDepositeAccount.displayAccountInfo();
-     System.out.println();
-
-     Account.bankName = "Bank Of Ceylon";
-
-        System.out.println("After changing the name:");
-        savingsAccount.displayAccountInfo();
-        System.out.println();
-        fixedDepositeAccount.displayAccountInfo();
-
-    }
 }
